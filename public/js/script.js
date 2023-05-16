52df31aefe077ad9ed882fe76a7d1a1aa494cc5d
MSiesto <miguelsiesto@gmail.com>
2023-05-16 21:29:43 +0200

    implementing maps API

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
   console.log("movie-spot JS imported successfully!");
 });
+
